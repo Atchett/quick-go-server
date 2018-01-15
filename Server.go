516c232b5f540d814ce707a9e7c89391fbdf81e5
Atchett <atchett@users.noreply.github.com>
2018-01-15 20:45:15 +0000

    Updated to add ability to read a JSON config file at a location of user choice.

--- Server.go
+++ Server.go
@@ -1,22 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
+// type for the JSON file values
+type Configuration struct {
+	Port          string
+	FolderToServe string
+}
+
 func main() {
-
-	port := flag.String("p", "8100", "port to serve on")
-	directory := flag.String("d", "/", "the directory of static file to host")
+	// read the command line params
+	configFileLocation := flag.String("c", "/", "config file location to use")
+	configFileName := flag.String("f", "config.json", "Filename for config, must be json format")
 	flag.Parse()
+	// create the config path variable
+	configFile := *configFileLocation + "/" + *configFileName
 
-	fs := http.FileServer(http.Dir(*directory))
+	// return the config to variables
+	port, directory := readConfig(configFile)
+
+	fs := http.FileServer(http.Dir(directory))
 	http.Handle("/", fs)
 
-	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+	log.Printf("Serving %s on HTTP port: %s\n", directory, port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func readConfig(fileName string) (string, string) {
+	// outputs the fileName to the console
+	log.Printf(fileName)
+	// opens the config file at the location passed in
+	file, _ := os.Open(fileName)
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	// decodes the JSON file into the type
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	// return the config file values
+	return configuration.Port, configuration.FolderToServe
 }
 
 // /Users/johnspurgin/Documents/Git/Grid/Arthaus
